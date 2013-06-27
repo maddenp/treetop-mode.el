ccbd991e74bd5380b6694ed416dffbffdba07cdb
Paul Madden <paul.a.madden@noaa.gov>
2013-06-27 16:37:12 -0600

    refactor in preparation for adding module & include handling

--- treetop-mode.el
+++ treetop-mode.el
@@ -12,6 +12,8 @@
 ;; See the License for the specific language governing permissions and
 ;; limitations under the License.
 
+(defvar w 2) ;; spaces per indentation level
+
 (require 'generic-x)
 
 (define-generic-mode 'treetop-mode-base
@@ -20,34 +22,44 @@
   '(("\\(<[A-Za-z0-9_]+>\\)" 1 'font-lock-variable-name-face)
     ("rule\\s-+\\([A-Za-z_][A-Za-z0-9_]*\\)\\s-*$" 1 'font-lock-function-name-face) 
     ("grammar\\s-+\\([A-Za-z_][A-Za-z0-9_]*\\)\\s-*$" 1 'font-lock-type-face)
-    ("include\\s-+\\(.+\\)" 1 'font-lock-variable-name-face)
     ("[^\\]'[^']*[^\\]'" . 'font-lock-string-face))
   nil
   nil
   "A base mode for Treetop grammar files")
 
-(defun end     () (looking-at "^\\s-*end[^A-Za-z0-9_]"))
-(defun grammar () (looking-at "^\\s-*grammar[^A-Za-z0-9_]"))
-(defun rule    () (looking-at "^\\s-*rule[^A-Za-z0-9_]"))
+(defun blank    () (looking-at "$"))
+(defun end      () (looking-at "end[^A-Za-z0-9_]"))
+(defun grammar  () (looking-at "grammar[^A-Za-z0-9_]"))
+(defun nonblank () (looking-at "[^:space:]"))
+(defun rule     () (looking-at "rule[^A-Za-z0-9_]"))
 
-(defun set-indent (iend irule igrammar)
-  (let ((searching t))
+(defun set-indent (offset-end offset-rule offset-grammar)
+  (let ((x -1))
     (save-excursion
-      (while searching
+      (while (= x -1)
         (forward-line -1)
-        (cond ((end)     (setq treetop-indent (funcall iend) searching nil))
-              ((rule)    (setq treetop-indent irule searching nil))
-              ((grammar) (setq treetop-indent igrammar searching nil))
-              ((bobp)    (setq searching nil)))))))
+        (skip-chars-forward "[:space:]")
+        (let ((c (current-indentation)))
+          (setq x (+ c (cond
+                        ((end) offset-end)
+                        ((rule) offset-rule)
+                        ((grammar) offset-grammar)
+                        ((bobp) (- c))
+                        (t (- (+ c 1)))))))))
+    x))
 
 (defun treetop-indent-line ()
   "Indent current line of Treetop grammar"
-  (setq treetop-indent 0)
   (beginning-of-line)
-  (cond ((rule)          (setq treetop-indent tab-width))
-        ((end)           (set-indent (lambda () 0) tab-width 0))
-        ((not (grammar)) (set-indent #'current-indentation (* tab-width 2) tab-width)))
-  (indent-line-to treetop-indent))
+  (skip-chars-forward "[:space:]")
+  (indent-line-to
+   (cond
+    ((blank) 0)
+    ((rule)     (set-indent (+ 0) (+ 0) (+ w)))
+    ((end)      (set-indent (- w) (+ 0) (+ 0)))
+    ((grammar)  (set-indent (+ 0) (+ 0) (+ 0)))
+    ((nonblank) (set-indent (+ 0) (+ w) (+ w)))
+    )))
 
 (define-derived-mode treetop-mode treetop-mode-base "Treetop"
   "A major mode for Treetop grammar files"
@@ -55,7 +67,7 @@
   (set (make-local-variable 'indent-line-function) 'treetop-indent-line)
   (set (make-local-variable 'indent-tabs-mode) nil)
   (set (make-local-variable 'tab-always-indent) t)
-  (set (make-local-variable 'tab-width) 2))
+  (set (make-local-variable 'tab-width) w))
 
 (add-to-list 'auto-mode-alist '("\\.tt\\'" . treetop-mode))
 
